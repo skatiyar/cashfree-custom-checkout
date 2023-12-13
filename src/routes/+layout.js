8b29b1e889aa81cb176d96270132c79ab8ed6c4b
skatiyar <katiyar.suyash@gmail.com>
2023-12-13 18:45:25 +0530

    added tailwind css

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,7 @@
-import { dev } from '$app/environment';
 import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+import { dev } from '$app/environment';
 
 inject({ mode: dev ? 'development' : 'production' });
 injectSpeedInsights();
